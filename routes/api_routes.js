215b63f464e2dc6a2aa21c4ba4b2cf1be3513924
Carolyn Smith <casmith205@gmail.com>
2018-04-17 16:28:32 -0500

    edited API routes and deleted the controller.js

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,54 +3,55 @@
 // Requiring our burger model
 var db = require("../models");
 // Requiring express
-var express = require ("express");
+var express = require("express");
 var app = express.Router();
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // GET route for getting all of the burgers
-  app.get("/", function(req, res) {
-    db.Burger.findAll({})
-      .then(function(dbPost) {
-        res.render("index", dbPost);
+  app.get("/", function (req, res) {
+    db.Burger.findAll({
+    })
+      .then(function (result) {
+        console.log(result);
+        res.render("index", result);
       });
   });
 
   // POST route for saving a new burger
-  app.post("/api/burgers", function(req, res) {
-    console.log(req.body);
-    db.Post.create({
+  app.post("/api/burgers", function (req, res) {
+    db.Burger.create({
       burger_name: req.body.burger_name,
       devoured: req.body.devoured
     })
-      .then(function(dbPost) {
-        res.json(dbPost);
+      .then(function (result) {
+        res.json(result);
       });
   });
 
   // DELETE route for deleting burgers
-//   app.delete("/api/burgers/:id", function(req, res) {
-//     db.Burger.destroy({
-//       where: {
-//         id: req.params.id
-//       }
-//     })
-//       .then(function(dbPost) {
-//         res.json(dbPost);
-//       });
-//   });
+  //   app.delete("/api/burgers/:id", function(req, res) {
+  //     db.Burger.destroy({
+  //       where: {
+  //         id: req.params.id
+  //       }
+  //     })
+  //       .then(function(dbPost) {
+  //         res.json(dbPost);
+  //       });
+  //   });
 
   // PUT route for updating burgers
-  app.put("/api/burgers/:id", function(req, res) {
+  app.put("/api/burgers/:id", function (req, res) {
     db.Post.update("Burgers",
       {
         where: {
           id: req.params.id
         }
       })
-      .then(function(dbPost) {
+      .then(function (dbPost) {
         res.json(dbPost);
       });
   });
