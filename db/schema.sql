5e72d1d334082d6f9360aafffb0a2bedcd03d087
Carolyn Smith <casmith205@gmail.com>
2018-04-17 14:13:07 -0500

    deleted orm and connection files. changed burger model

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,5 @@
 -- CREATE & USE DB
-CREATE DATABASE burgers_db;
+CREATE DATABASE burgers_sequelize_db;
 
-USE burgers_db;
-
--- CREATE THE BURGERS TABLE
-CREATE TABLE burgers (
-    id INT AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR(255),
-    devoured BOOLEAN,
-    createdAt TIMESTAMP NOT NULL,
-    PRIMARY KEY (id)
-);
+USE burgers_sequelize_db;
 
--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,17 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
-
-// Creating a burger object with three functions that mirror the ORM set up with a specified table
-var burger = {
-    selectAll: function(cb){
-        orm.selectAll("burgers", function(res){
-            cb(res);
-        });
-    },
-    insertOne: function(values, cb){
-        orm.insertOne("burgers", "burger_name", values, function(res){
-            cb(res);
-        });
-    },
-    updateOne: function(col, condition, cb){
-        orm.updateOne("burgers", col, condition, function(res){
-            cb(res);
-        })
-    }
-};
-
-// Export the database functions for the controller (burgers_controller.js)
-module.exports = burger;
+module.exports = function(sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+      burger_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      devoured: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+    });
+    return Burger;
+  };
+  
